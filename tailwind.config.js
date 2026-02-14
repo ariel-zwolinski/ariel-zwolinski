42d0f8a5aa46df57f2920e791c16724c86e8ec16
Ariel Zwolinski <97000368+ariel-zwolinski@users.noreply.github.com>
2026-02-14 22:04:20 +0100

    Rename calculator to TSX and simplify Tailwind content paths

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,html}", "./kalkulator.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
